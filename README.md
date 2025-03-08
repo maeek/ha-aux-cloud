d426ad46e887c968835cebb3a62e57ca621da6f2
maczet <maczet@pm.me>
2025-03-08 07:44:08 +0100

    autopep8 format fixed.

--- README.md
+++ README.md
@@ -2,36 +2,44 @@
 
 (**Currently work in progress**)
 
-Unofficial integration for Aux Cloud connected appliances like air conditioners and heat pumps. Aux Cloud is a service based on the Broadlink platform that allows you to control your appliances from anywhere. This is a cloud alternative to [replacing wifi module in your AC](https://github.com/GrKoR/esphome_aux_ac_component), which will also allow you to control heat pumps. The implementation of API requests are based on public resources from Broadlink documentation and lots of reverse engineering.
+Unofficial integration for Aux Cloud connected appliances like air conditioners and heat pumps. Aux Cloud is a service
+based on the Broadlink platform that allows you to control your appliances from anywhere. This is a cloud alternative
+to[replacing wifi module in your AC](https: // github.com / GrKoR / esphome_aux_ac_component), which will also allow you
+to control heat pumps. The implementation of API requests are based on public resources from Broadlink documentation and
+lots of reverse engineering.
 
-## demo
+# demo
+
 How to use the demo script:
-1. Create file config.yaml in custom_components/dev directory
+
+1. Create file config.yaml in custom_components / dev directory
 2. Complete in folder dev config.yaml with your email and password, and shared value
 3. Example of config.yaml
+
 ```yaml
 email: 'email'
 password: 'password'
 shared: False
 ```
+
 4. Run script with `python demo.py`
 
-## TODO
+# TODO
 
 As the project is in early stage, there are a lot of things to do. Here is a list of tasks that need to be done:
 
 - [x] Reverse egineer the Aux Cloud API
-- [x] [API] Implement login
-- [x] [API] Implement getting devices information
-- [ ] [API] Implement updating device state
-- [ ] [Home Assistant] Cloud data fetcher
-- [ ] [Home Assistant] Data coordinator
-- [ ] [Home Assistant] config flow
-- [ ] [Home Assistant] climate entity
-- [ ] [Home Assistant] sensor entity
-- [ ] [Home Assistant] binary sensor entity
-- [ ] [Home Assistant] number entity
-- [ ] [Home Assistant] services
-- [ ] Documentation
-- [ ] Add to HACS
-- [ ] Translations
+  - [x][API] Implement login
+  - [x][API] Implement getting devices information
+- [][API] Implement updating device state
+- [][Home Assistant] Cloud data fetcher
+- [][Home Assistant] Data coordinator
+- [][Home Assistant] config flow
+- [][Home Assistant] climate entity
+- [][Home Assistant] sensor entity
+- [][Home Assistant] binary sensor entity
+- [][Home Assistant] number entity
+- [][Home Assistant] services
+- [] Documentation
+- [] Add to HACS
+- [] Translations
--- custom_components/aux-cloud/api/demo.py
+++ custom_components/aux-cloud/api/demo.py
@@ -10,53 +10,56 @@ from const import POWER_OFF, HEATING, TEMP, COOLING, FAN_SPEEDS_LOW, FAN_SPEEDS_
 
 
 def get_config_path():
-  current_dir = pathlib.Path(__file__).parent
-  custom_components_dir = current_dir.parent.parent
-  return os.path.join(custom_components_dir, 'dev', 'config.yaml')
+    current_dir = pathlib.Path(__file__).parent
+    custom_components_dir = current_dir.parent.parent
+    return os.path.join(custom_components_dir, 'dev', 'config.yaml')
+
 
 if __name__ == "__main__":
 
-  with open(get_config_path(), "r", encoding="utf-8") as f:
-    config = yaml.load(f, Loader=yaml.FullLoader)
-  email: str = config['email']
-  password: str = config['password']
-  shared: bool = config['shared']
+    with open(get_config_path(), "r", encoding="utf-8") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    email: str = config['email']
+    password: str = config['password']
+    shared: bool = config['shared']
 
-  # Example usage
-  async def main():
-    cloud = AuxCloudAPI()
-    await cloud.login(email, password)
 
-    families = await cloud.list_families()
-    for family in families:
-      print(f"FamilyId {family['familyid']}:")
-      devices = await cloud.list_devices(family['familyid'], shared)
-      if devices:
-        print(f"Devices:")
-        pprint.pprint(devices)
-        for device in devices:
+    # Example usage
 
-          state = await cloud.query_device_state(
-            device['endpointId'],
-            device['devSession']
-          )
-          print("Device state:")
-          pprint.pprint(state)
-          print(f"devSession {device['devSession']}")
-          params = await cloud.get_device_params(device)
-          print(f"Device params:")
-          pprint.pprint(params)
-          # await cloud.set_device_params(device, POWER_OFF)
-          # await cloud.set_device_params(device, HEATING)
-          # await cloud.set_device_params(device, TEMP)
-          await cloud.set_device_params(device, FAN_SPEEDS_HIGH)
+    async def main():
+        cloud = AuxCloudAPI()
+        await cloud.login(email, password)
 
-          params = await cloud.get_device_params(device)
+        families = await cloud.list_families()
+        for family in families:
+            print(f"FamilyId {family['familyid']}:")
+            devices = await cloud.list_devices(family['familyid'], shared)
+            if devices:
+                print(f"Devices:")
+                pprint.pprint(devices)
+                for device in devices:
+                    state = await cloud.query_device_state(
+                        device['endpointId'],
+                        device['devSession'])
 
-          print(f"Device params after set:")
-          pprint.pprint(params)
+                    print("Device state:")
+                    pprint.pprint(state)
+                    print(f"devSession {device['devSession']}")
+                    params = await cloud.get_device_params(device)
+                    print(f"Device params:")
+                    pprint.pprint(params)
+                    # await cloud.set_device_params(device, POWER_OFF)
+                    # await cloud.set_device_params(device, HEATING)
+                    # await cloud.set_device_params(device, TEMP)
+                    await cloud.set_device_params(device, FAN_SPEEDS_HIGH)
 
-        print("")
+                    params = await cloud.get_device_params(device)
 
-  loop = asyncio.get_event_loop()
-  loop.run_until_complete(main())
+                    print(f"Device params after set:")
+                    pprint.pprint(params)
+
+                print("")
+
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
