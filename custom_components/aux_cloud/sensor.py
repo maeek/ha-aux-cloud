74face961f1d7d5799bb289d525503b85934cb42
maczet <maczet@pm.me>
2025-03-25 10:42:57 +0100

    Revert "debug logs"
    This reverts commit 661515608349d872eea4ab819103750f4459c098.

--- custom_components/aux_cloud/sensor.py
+++ custom_components/aux_cloud/sensor.py
@@ -1,9 +1,8 @@
 """Support for AUX Cloud sensors."""
 from __future__ import annotations
 
-from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
+from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import UnitOfTemperature
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -12,147 +11,46 @@ from .const import DOMAIN, _LOGGER
 
 
 async def async_setup_entry(
-        hass: HomeAssistant,
-        config_entry: ConfigEntry,
-        async_add_entities: AddEntitiesCallback,
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up AUX Cloud sensor platform."""
     data = hass.data[DOMAIN]
-
-    if not data.devices:
-        _LOGGER.warning("No AUX Cloud devices found")
+    
+    if not data.aux_cloud.thermostats:
+        _LOGGER.error("No AUX Cloud devices found")
         return
-
+    
     entities = []
-
-    # Add sensor entities for each device
-    for device in data.devices:
-        # Skip devices that are offline
-        if device.get('state', 0) != 1:
-            continue
-
-        # Add temperature sensors if the device has temperature parameters
-        params = device.get('params', {})
-
-        # Check if this is a heat pump with tank temperature
-        if 'hp_water_tank_temp' in params:
-            entities.append(AuxCloudWaterTankSensor(data, device))
-
-        # Add room temperature sensor if available
-        if 'envtemp' in params:
-            entities.append(AuxCloudTemperatureSensor(data, device))
-
-        # For devices with AC temperature setpoint
-        if 'ac_temp' in params:
-            entities.append(AuxCloudSetpointSensor(data, device))
-
-        # You can add more sensor types based on your device parameters
-
-    if entities:
-        _LOGGER.debug("Adding %d sensor entities", len(entities))
-        async_add_entities(entities, True)
-    else:
-        _LOGGER.warning("No AUX Cloud sensor entities created")
+    
+    # Add sensor entities for each thermostat
+    for thermostat in data.aux_cloud.thermostats:
+        entities.append(AuxCloudTemperatureSensor(data, thermostat))
+        # Add other sensor types as needed
+    
+    async_add_entities(entities, True)
 
 
 class AuxCloudTemperatureSensor(SensorEntity):
     """Representation of an AUX Cloud temperature sensor."""
 
-    def __init__(self, data, device):
+    def __init__(self, data, thermostat):
         """Initialize the sensor."""
         self._data = data
-        self._device = device
-        self._attr_unique_id = f"{device['endpointId']}_temperature"
-        self._attr_name = f"{device.get('friendlyName', 'AUX Device')} Temperature"
-        self._attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
-        self._attr_device_class = SensorDeviceClass.TEMPERATURE
-
+        self._thermostat = thermostat
+        self._attr_unique_id = f"{thermostat.id}_temperature"
+        self._attr_name = f"{thermostat.name} Temperature"
+        self._attr_native_unit_of_measurement = "°C"
+        self._attr_device_class = "temperature"
+        
     @property
     def native_value(self):
         """Return the state of the sensor."""
-        params = self._device.get('params', {})
-        # Some devices store temperature as tenths of degrees
-        if 'envtemp' in params:
-            temp = params['envtemp']
-            # If temperature is stored as an integer but represents tenths of degrees
-            if temp > 100:
-                return temp / 10
-            return temp
-        return None
-
+        # This should return the current temperature from your API
+        # Modify this to match your actual API data structure
+        return self._thermostat.temperature
+        
     async def async_update(self):
         """Get the latest data."""
         await self._data.update()
-
-        # Find the updated device data
-        for device in self._data.devices:
-            if device['endpointId'] == self._device['endpointId']:
-                self._device = device
-                break
-
-
-class AuxCloudWaterTankSensor(SensorEntity):
-    """Representation of an AUX Cloud water tank temperature sensor."""
-
-    def __init__(self, data, device):
-        """Initialize the sensor."""
-        self._data = data
-        self._device = device
-        self._attr_unique_id = f"{device['endpointId']}_water_tank_temperature"
-        self._attr_name = f"{device.get('friendlyName', 'AUX Device')} Water Tank Temperature"
-        self._attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
-        self._attr_device_class = SensorDeviceClass.TEMPERATURE
-
-    @property
-    def native_value(self):
-        """Return the state of the sensor."""
-        params = self._device.get('params', {})
-        if 'hp_water_tank_temp' in params:
-            return params['hp_water_tank_temp']
-        return None
-
-    async def async_update(self):
-        """Get the latest data."""
-        await self._data.update()
-
-        # Find the updated device data
-        for device in self._data.devices:
-            if device['endpointId'] == self._device['endpointId']:
-                self._device = device
-                break
-
-
-class AuxCloudSetpointSensor(SensorEntity):
-    """Representation of an AUX Cloud temperature setpoint sensor."""
-
-    def __init__(self, data, device):
-        """Initialize the sensor."""
-        self._data = data
-        self._device = device
-        self._attr_unique_id = f"{device['endpointId']}_setpoint"
-        self._attr_name = f"{device.get('friendlyName', 'AUX Device')} Target Temperature"
-        self._attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
-        self._attr_device_class = SensorDeviceClass.TEMPERATURE
-        self._attr_entity_category = EntityCategory.DIAGNOSTIC
-
-    @property
-    def native_value(self):
-        """Return the state of the sensor."""
-        params = self._device.get('params', {})
-        if 'ac_temp' in params:
-            temp = params['ac_temp']
-            # If temperature is stored as an integer but represents tenths of degrees
-            if temp > 100:
-                return temp / 10
-            return temp
-        return None
-
-    async def async_update(self):
-        """Get the latest data."""
-        await self._data.update()
-
-        # Find the updated device data
-        for device in self._data.devices:
-            if device['endpointId'] == self._device['endpointId']:
-                self._device = device
-                break
