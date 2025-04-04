1f7834c07741cff77e01fd8fb608994d153300ae
maeek <39914671+maeek@users.noreply.github.com>
2025-04-04 13:57:17 +0200

    feat: select for quiet mode in heat pump

---
+++ custom_components/aux_cloud/select.py
@@ -0,0 +1,98 @@
+"""Select platform for AUX Cloud integration."""
+
+from homeassistant.components.select import SelectEntity, SelectEntityDescription
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+
+from custom_components.aux_cloud.api.const import HP_QUIET_MODE
+
+from .const import DOMAIN, _LOGGER
+from .util import BaseEntity
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the AUX select platform."""
+    data = hass.data[DOMAIN][entry.entry_id]
+    coordinator = data["coordinator"]
+
+    entities = []
+
+    # Create select entities for each device
+    for device in coordinator.data["devices"]:
+        if "params" in device:
+            if HP_QUIET_MODE in device["params"]:
+                entities.append(
+                    AuxSelectEntity(
+                        coordinator,
+                        device["endpointId"],
+                        SelectEntityDescription(
+                            key=HP_QUIET_MODE,
+                            name="Quiet Mode",
+                            icon="mdi:volume-mute",
+                            translation_key="aux_select_qtmode",
+                        ),
+                        {
+                            "off": {
+                                "value": 0,
+                                "icon": "mdi:volume-high",
+                            },
+                            "quiet_1": {
+                                "value": 1,
+                                "icon": "mdi:volume-off",
+                            },
+                            "quiet_2": {
+                                "value": 2,
+                                "icon": "mdi:volume-mute",
+                            },
+                        },
+                    )
+                )
+
+    if entities:
+        async_add_entities(entities, True)
+    else:
+        _LOGGER.info("No AUX select devices added")
+
+
+class AuxSelectEntity(BaseEntity, CoordinatorEntity, SelectEntity):
+    """AUX Cloud select entity."""
+
+    def __init__(
+        self,
+        coordinator,
+        device_id,
+        entity_description: SelectEntityDescription,
+        options,
+    ):
+        super().__init__(coordinator, device_id, entity_description)
+        self._options = options
+        self._attr_options = list(options.keys())
+        self._attr_current_option = self._get_device_params().get(
+            self.entity_description.key, None
+        )
+
+    @property
+    def current_option(self):
+        options_reverse = {v["value"]: k for k, v in self._options.items()}
+        return options_reverse.get(
+            self._get_device_params().get(self.entity_description.key, None)
+        )
+
+    @property
+    def icon(self):
+        return self._options.get(self.current_option, {}).get("icon", None)
+
+    async def async_select_option(self, option: str):
+        new_option = self._options.get(option).get("value", None)
+        if option not in self._attr_options:
+            _LOGGER.error(f"Invalid option selected: {option}={new_option}")
+            return
+
+        await self._set_device_params({self.entity_description.key: new_option})
+        await self.coordinator.async_request_refresh()
