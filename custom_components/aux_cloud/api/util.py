a530f300f5e9273ece91f48f2f6b8ecfd7a37ec7
Maciej Suchanecki <39914671+maeek@users.noreply.github.com>
2025-04-06 10:35:26 +0200

    Climate entity for ACs, switch entity, black formatter (#10)
    * feat: working climate
    
    * Use Black formatter as autopep8 is not working correctly in vscode
    
    * Update consts
    
    * feat: select for quiet mode in heat pump
    
    * fix: Black formatter update
    
    * Update README
    
    * Added switch entity
    
    * Update README
    
    * Move files
    
    * Update README
    
    * Cleaning
    
    * Translations
    
    * Update README
    
    * Added info about code formatting to README

--- custom_components/aux_cloud/api/util.py
+++ custom_components/aux_cloud/api/util.py
@@ -7,7 +7,7 @@ def encrypt_aes_cbc_zero_padding(iv: bytes, key: bytes, data: bytes):
     try:
         cipher = AES.new(key, AES.MODE_CBC, iv)
         padded_data = data
-        padded_data += b'\x00' * (AES.block_size - len(data) % AES.block_size)
+        padded_data += b"\x00" * (AES.block_size - len(data) % AES.block_size)
         encrypted_data = cipher.encrypt(padded_data)
         return encrypted_data
     except Exception as e:
